836ce0d7d9cba7701d4a761dbba11300d907e67a
Camilo Cortes <camilocortes@Camilos-MacBook-Pro.local>
2024-10-18 19:41:39 -0500

    second-commit back to normal

---
+++ src/lib/config.ts
@@ -0,0 +1,8 @@
+import { dev } from '$app/environment'
+
+export const title = 'coretekne';
+export const description = 'coredev Blog';
+export const url = dev ? 'http://localhost:5173/' :
+'/';
+
+
---
+++ src/routes/header.svelte
@@ -0,0 +1,60 @@
+<script lang='ts'>
+  import * as config from '$lib/config'
+</script>
+
+<nav>
+  <a href="/" class="title">
+    <b>{config.title}</b>
+  </a>
+
+  <ul class='links'>
+    <li>
+      <a href="/us">This is us</a>
+    </li>
+    <li>
+      <a href="/contacto">contacto</a>
+    </li> 
+    <li>
+      <a href="/rss.xml" target="_blank">rss</a>
+    </li>
+  </ul>
+
+  <button>
+    toggle
+  </button>
+
+</nav>
+
+<style>
+
+  nav{
+    padding-block: var(--size-7);
+  } 
+
+  .links {
+    margin-block: var(--size-7);
+  }
+
+  a{
+    color: inherit;
+    text-decoration: none;
+  }
+
+  @media (min-width: 768px) {
+
+    nav {
+      display: flex;
+      justify-content:space-between;
+
+    }
+      
+    .links {
+      display: flex;
+      gap: var(--size-7);
+      margin-block: 0;
+    }
+  }
+
+
+  
+</style>
